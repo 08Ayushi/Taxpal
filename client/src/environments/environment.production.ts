bbe30b623af49dec3f798c13527890c24affb625
Ayushi <145868166+08Ayushi@users.noreply.github.com>
2025-10-31 22:54:10 +0530

    Update environment.production.ts

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // replace after Render is live:
-  API_URL: 'https://RENDER_API_URL_GOES_HERE/api/v1'
+  API_URL: 'https://taxpal-5.onrender.com/api/v1'
 };
